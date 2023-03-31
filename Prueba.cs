c713b45d9d8194b91d6633ab90ed32a770d47399
juvenalhector <juvenalhector@gmail.com>
2023-03-30 21:03:05 -0400

    Create Prueba.cs

---
+++ Prueba.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiGitHub
+{
+    public class Prueba
+    {
+        
+        public prueba()
+        {
+            Console.WriteLine("Ejemplo de una prueba");
+        }
+
+    }
+}
